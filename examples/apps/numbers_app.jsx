666f02c80f63b1c5d31d71970a62a80d84b869d8
Catalin Enache <catalinenache78@gmail.com>
2016-01-17 14:55:43 +0200

    add info to numbersApp

--- examples/apps/numbers_app.jsx
+++ examples/apps/numbers_app.jsx
@@ -23,6 +23,33 @@ class NumbersApp extends React.Component {
 			<div>
 				<h3>InputNumbers</h3>
                 <pre>{`
+<InputNumber name="number_1"
+	 start={-1}
+	 onChange={this.numberOnChange.bind(this)}
+	 debug={false} />
+<InputNumber name="number_2"
+	 value={this.state.numberStringValue}
+	 start={-1}
+	 onChange={this.numberOnChange.bind(this)}
+	 debug={false} />
+<InputNumber name="number_3"
+	 defaultValue={'-1'}
+	 start={-1}
+	 onChange={this.numberOnChange.bind(this)}
+	 debug={false} />
+{/* height will be constrained to min 20 */}
+<InputNumber name="number_4"
+	 start={-100000000099}
+	 end={100000000099}
+	 step={1}
+	 disabled={false}
+	 style={{width: '150px', height: '15px'}}
+	 debug={true} />
+{/* height will be constrained to max 50 */}
+<InputNumber name="number_5"
+	 disabled={true}
+	 style={{width: '250px', height: '60px', fontSize: '30px', fontStyle: 'italic', fontWeight: 'bold'}}
+	 debug={false} />
                 `}
                 </pre>
 				<br />
